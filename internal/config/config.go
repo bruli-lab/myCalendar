b0f4ee9ff81d61f1b49e620adfa1dc041aa48a5e
Pablo Braulio <brulics@gmail.com>
2026-05-12 22:04:49 +0200

    Removing telegram env vars

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,19 +3,17 @@ package config
 import "github.com/caarlos0/env/v11"
 
 type Config struct {
-	ClientID       string `env:"CLIENT_ID,required"`
-	ClientSecret   string `env:"CLIENT_SECRET,required"`
-	CallbackHost   string `env:"CALLBACK_HOST,required"`
-	CallbackURL    string `env:"CALLBACK_URL,required"`
-	TokensFile     string `env:"TOKENS_FILE,required"`
-	TelegramToken  string `env:"TELEGRAM_TOKEN,required"`
-	TelegramChatID int    `env:"TELEGRAM_CHAT_ID,required"`
-	ServerHost     string `env:"SERVER_HOST,required"`
-	LogLevel       string `env:"LOG_LEVEL,required"`
-	NtfyURL        string `env:"NTFY_URL,required"`
-	NtfyTopic      string `env:"NTFY_TOPIC,required"`
-	NtfyUser       string `env:"NTFY_USER,required"`
-	NtfyPassword   string `env:"NTFY_PASSWORD,required"`
+	ClientID     string `env:"CLIENT_ID,required"`
+	ClientSecret string `env:"CLIENT_SECRET,required"`
+	CallbackHost string `env:"CALLBACK_HOST,required"`
+	CallbackURL  string `env:"CALLBACK_URL,required"`
+	TokensFile   string `env:"TOKENS_FILE,required"`
+	ServerHost   string `env:"SERVER_HOST,required"`
+	LogLevel     string `env:"LOG_LEVEL,required"`
+	NtfyURL      string `env:"NTFY_URL,required"`
+	NtfyTopic    string `env:"NTFY_TOPIC,required"`
+	NtfyUser     string `env:"NTFY_USER,required"`
+	NtfyPassword string `env:"NTFY_PASSWORD,required"`
 }
 
 func New() (*Config, error) {
